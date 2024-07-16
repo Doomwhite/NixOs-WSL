256dc001c5210fbf81fc3b6dbf3d0e65d0a87561
Doomwhite <doomwhitex@gmail.com>
2024-07-15 21:15:06 -0300

    feat(git): add aliases

--- home.nix
+++ home.nix
@@ -152,11 +152,22 @@ in {
       userEmail = "doomwhitex@gmail.com";
       userName = "Doomwhite";
       extraConfig = {
-        # FIXME: uncomment the next lines if you want to be able to clone private https repos
         url = {
           "https://oauth2:${secrets.github_token}@github.com" = {
             insteadOf = "https://github.com";
           };
+        };
+        core = {
+          longspaths = true;
+          preloadindex = true;
+          fscache = true;
+          defaultbranch = "main";
+        };
+        fetch = {
+          prune = true;
+        };
+        worktree = {
+          guessRemote = true;
         };
         push = {
           default = "current";
@@ -169,7 +180,7 @@ in {
           colorMoved = "default";
         };
         safe = {
-          directory = "~/configuration";
+          directory = [ "~/configuration" "~/${emacsDir}" ];
         };
         alias = {
           br = "branch";
@@ -193,6 +204,37 @@ in {
           cmp = "commit -p";
           cmpm = "commit -p -m";
           co = "checkout";
+          ps = "push";
+          psf = "push --force";
+          psu = "push -u";
+          psup = "!f() { \
+            current_branch=$(git rev-parse --abbrev-ref HEAD); \
+                remote_branch=\"$current_branch\"; \
+                if ! git rev-parse --abbrev-ref --symbolic-full-name @{u} >/dev/null 2>&1; then \
+                    remote_branch=\"--set-upstream origin $current_branch\"; \
+                fi; \
+                git push $remote_branch; \
+          }; f";
+          rao = "remote add origin";
+          rb = "rebase";
+          rba = "rebase --abort";
+          rbc = "rebase --continue";
+          st = "status";
+          sm = "submodule";
+          sma = "submodule add";
+          smf = "submodule foreach --recursive";
+          sw = "switch";
+          swd = "switch dev";
+          swm = "switch master";
+          up = "!git fetch && git status";
+          uppl = "!git up && git pull";
+          wt = "worktree";
+          wta = "worktree add";
+          wtl = "worktree list";
+          wtr = "worktree remove";
+          wtatb = "!f() { git worktree add --track -b $1 $1 origin/$2; }; f";
+          wtab = "!f() { git worktree add -b $1 $1 $2; }; f";
+          lsw = "switch -";
         };
       };
     };
